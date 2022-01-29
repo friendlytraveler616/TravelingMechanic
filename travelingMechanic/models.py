dfc90e89e6d72a747dfbb189772185c101d6795a
Xian Jian Zhang <xianjianzhang.yahoo@gmail.com>
2022-01-29 15:43:57 -0500

    Modify the model values to float field, created signals for the review and web user model

--- travelingMechanic/models.py
+++ travelingMechanic/models.py
@@ -7,15 +7,15 @@ from django.contrib.auth.models import User
 class webUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profilePic = models.ImageField(default='default.png', upload_to='profilePic')
-    rating = models.IntegerField(default=0,
-                                  validators=[validators.MinValueValidator(1), validators.MaxValueValidator(5)])
+    rating = models.FloatField(default=0,
+                                  validators=[validators.MinValueValidator(0), validators.MaxValueValidator(5)])
 
 class Commission(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=300, blank=True)
-    lat = models.IntegerField(default=-82.374);
-    long = models.IntegerField(default=29.648);
-    askPrice = models.IntegerField(default=0, validators=[validators.MinValueValidator(0)])
+    lat = models.FloatField(default=-82.374);
+    long = models.FloatField(default=29.648);
+    askPrice = models.FloatField(default=0, validators=[validators.MinValueValidator(0)])
     images = models.ImageField(default='default.png', upload_to='commissionpic/')
     date_created = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(webUser, on_delete=models.CASCADE)
