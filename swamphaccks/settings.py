06621aa7d5cce19503b7b20c461b14dfb4fc3768
Patrick-Lapid <patrickklapid@gmail.com>
2022-01-28 20:58:28 -0500

    add app

--- swamphaccks/settings.py
+++ swamphaccks/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django.apps
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'travelingMechanic'
 ]
 
 MIDDLEWARE = [
