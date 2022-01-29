f10672fd6530b3c3e406bde8b65f8b66f03665ae
Xian Jian Zhang <xianjianzhang.yahoo@gmail.com>
2022-01-29 12:07:12 -0500

    Created commission model, migrated it, add long/lat, now working on implementing profile and user

--- travelingMechanic/models.py
+++ travelingMechanic/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 from django.utils import timezone
+from django.core import validators
 from django.contrib.auth.models import User
 
 # Create your models here.
 class Commission(models.Model):
-    title = models.CharField(max_length=20)
-    description = models.CharField(max_length=100)
-    askPrice = models.IntegerField
-    images = models.ImageField
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=300, blank=False)
+    lat = models.IntegerField(default=-82.374);
+    long = models.IntegerField(default=29.648);
+    askPrice = models.IntegerField(default=0, validators=[validators.MinValueValidator(0)])
+    images = models.ImageField(default='default.png', upload_to='commissionpic/')
     date_created = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return ('\nTitle: ' + self.title + '\nDescription: ' + self.description
+                + '\nAsk price: ' + str(self.askPrice) + '\nUser: ' + self.author.username)
+
+# class webUser(User):
+#     class
