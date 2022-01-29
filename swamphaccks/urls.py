0dfdf4ee1fe4c11b2be56275a1e698eae89780eb
Xian Jian Zhang <xianjianzhang.yahoo@gmail.com>
2022-01-28 23:35:02 -0500

    Added static folder for css and image. Also added bootstrap

--- swamphaccks/urls.py
+++ swamphaccks/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('travelingMechanic.urls')),
     path('user/',include('user.urls')),
     path('payment/', include('payment.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
