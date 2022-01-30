629137e864255905fb5150bda4fbb6f6ccd69839
Xian Jian Zhang <xianjianzhang.yahoo@gmail.com>
2022-01-29 22:21:44 -0500

    Create forms (update) for user/webuser, create form for creating reviews, added author to review, added done attribute to commission, created a creation form for review, created a html page for review

--- user/views.py
+++ user/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegistForm
+from .forms import UserRegistForm, userUpdateForm, webUserUpdateForm
 # Create your views here.
 def profile(request):
-    context = {"title":"profile"}
+    #Updating requires instance
+    if (request.method == 'POST'):
+        uForm = userUpdateForm(request.POST, instance=request.user)
+        wForm = webUserUpdateForm(request.POST, request.FILES,  instance=request.user.webuser)
+        print(request.FILES)
+        if (uForm.is_valid() and wForm.is_valid()):
+            uForm.save()
+            wForm.save()
+    else:
+        uForm = userUpdateForm(instance=request.user)
+        wForm = webUserUpdateForm(instance=request.user.webuser)
+    context = {"title":"profile", "uForm":uForm, "wForm":wForm}
     return render(request, 'user/profile.html', context)
 
 def register(request):
@@ -13,7 +24,7 @@ def register(request):
             return redirect('user-login')
     else:
         form = UserRegistForm()
-    context = {"title":"Registration", 'form':UserRegistForm}
+    context = {"title":"Registration", 'form':form}
     return render(request, 'user/register.html', context)
 
 def forgetPW(request):
