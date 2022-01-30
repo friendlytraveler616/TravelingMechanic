5bf45f49251c36dcdf94bbaed48411ecefaf7cd9
Xian Jian Zhang <xianjianzhang.yahoo@gmail.com>
2022-01-30 09:50:30 -0500

    changed the setting.py

--- swamphaccks/settings.py
+++ swamphaccks/settings.py
@@ -148,12 +148,12 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'travelingmechanictheog'
 EMAIL_HOST_PASSWORD = 'Xjz769189317?'
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY  = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY  = os.environ.get('AWS_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 #Don't overwrite file of same name
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL=None
-
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL=None
+#
+# DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
