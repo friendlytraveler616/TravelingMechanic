3d9dcbb462d5faf5fdc61a98c03999508b5e49ed
Xian Jian Zhang <xianjianzhang.yahoo@gmail.com>
2022-01-31 10:34:09 -0500

    More security patches, and modified files where the variables are utilized

--- swamphaccks/settings.py
+++ swamphaccks/settings.py
@@ -150,3 +150,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
+#Google API keys
+API_KEY_URL = os.environ.get('API_KEY_URL')
+
